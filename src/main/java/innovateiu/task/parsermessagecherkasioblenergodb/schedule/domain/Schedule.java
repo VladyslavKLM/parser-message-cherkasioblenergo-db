ea2a119d6056afe62fdd0df91e76feb8eaf72d19
Vladyslav Klymchuk <klymchuk.vladyslav@gmail.com>
2024-09-25 21:28:21 +0300

    +

--- src/main/java/innovateiu/task/parsermessagecherkasioblenergodb/schedule/domain/Schedule.java
+++ src/main/java/innovateiu/task/parsermessagecherkasioblenergodb/schedule/domain/Schedule.java
@@ -1,7 +1,33 @@
-package innovateiu.task.parsermessagecherkasioblenergodb;
+package innovateiu.task.parsermessagecherkasioblenergodb.schedule.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDate;
+
+@Getter
+@Setter
 @Entity
+@Table(name = "schedule")
 public class Schedule {
-  }
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "schedule_id_gen")
+    @SequenceGenerator(name = "schedule_id_gen", sequenceName = "schedule_id_seq", allocationSize = 1)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotNull
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    @NotNull
+    @Column(name = "hour", nullable = false, length = Integer.MAX_VALUE)
+    private String hour;
+
+    @NotNull
+    @Column(name = "number_turn", nullable = false)
+    private Long numberTurn;
+
+}
